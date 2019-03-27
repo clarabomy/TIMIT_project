99b4a51fca3d2046f939bb4b32a678dd42519ac8
Unknown <cbclara1309@gmail.com>
2019-03-27 11:14:41 +0100

    update utils

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import pyaudio
+import wave
 import librosa as lr
 
 def plot_learning_curve(est, X_train, y_train) :
@@ -113,5 +115,38 @@ def model_curves(model):
     plt.title('Training and validation loss')
     plt.legend()
     plt.show()
+
+def record(audio_filename):
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 5
+
+    audio = pyaudio.PyAudio()
+
+    # start Recording
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                    rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+    print("recording...")
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("finished recording")
+
+    # stop Recording
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    waveFile = wave.open(audio_filename, 'wb')
+    waveFile.setnchannels(CHANNELS)
+    waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+    waveFile.setframerate(RATE)
+    waveFile.writeframes(b''.join(frames))
+    waveFile.close()
 
 print("utils.py correctly charged")
